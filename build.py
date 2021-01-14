449019688ec47e1cf8d299f2eabb26ca9ff5a338
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-01-14 06:09:22 +0300

    fix build.py

--- build.py
+++ build.py
@@ -3,14 +3,22 @@
 
 
 from cpt.packager import ConanMultiPackager
+import os
+
+
+env_pure_c = os.getenv("CONAN_PURE_C", True)
+pure_c = True if str(env_pure_c).lower() != "false" else False
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(
         exclude_vcvars_precommand=True
     )
-    builder.add_common_builds()
+    builder.add_common_builds(
+        pure_c=pure_c
+    )
     builder.remove_build_if(
         lambda build: build.settings.get("compiler.libcxx") == "libstdc++"
     )
     builder.run()
+
