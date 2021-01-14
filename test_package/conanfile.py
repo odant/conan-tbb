5705d98981bb5d6d1ae10cb8505a43747638b0e5
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-01-14 15:08:16 +0300

    Upgrade test package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,5 +1,5 @@
-# Test for Intel TBB Conan package
-# Dmitriy Vetutnev, Odant, 2018
+# Test Conan package
+# Dmitriy Vetutnev, Odant 2019 - 2020
 
 
 from conans import ConanFile, CMake
@@ -8,24 +8,23 @@ from conans import ConanFile, CMake
 class PackageTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-
-    def build(self):
-        if self.options["tbb"].built_in_tests:
-            return
-        cmake = CMake(self)
-        cmake.verbose = True
-        cmake.configure()
-        cmake.build()
+    build_requires = "ninja/1.9.0"
 
     def imports(self):
         self.copy("*.pdb", dst="bin", src="bin")
         self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.so.*", dst="bin", src="lib")
+        self.copy("*.so*", dst="bin", src="lib")
+
+    def build(self):
+        cmake = CMake(self, generator="Ninja", msbuild_verbosity='normal')
+        cmake.verbose = True
+        cmake.configure()
+        cmake.build()
+        self.cmake_is_multi_configuration = cmake.is_multi_configuration
 
     def test(self):
-        if self.options["tbb"].built_in_tests:
-            return
-        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+        if self.cmake_is_multi_configuration:
             self.run("ctest --verbose --build-config %s" % self.settings.build_type)
         else:
             self.run("ctest --verbose")
+
