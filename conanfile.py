223c68a385f1047bf1aa3aad942113773e72b435
d0nc1h0t <udincev@odant.ru>
2025-04-28 12:49:49 +0300

    Adapted to Conan v2

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,11 @@
 # Intel TBB Conan package
 # Dmitriy Vetutnev, Odant, 2018
 
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanException
-import os, glob
+from conan import ConanFile, tools
+from conan.errors import ConanInvalidConfiguration, ConanException
+from datetime import datetime
+import os, glob, shutil
 
-
-def get_safe(options, name):
-    try:
-        return getattr(options, name, None)
-    except ConanException:
-        return None
 
 
 class TBBConan(ConanFile):
@@ -19,92 +14,92 @@ class TBBConan(ConanFile):
     license = "Apache License 2.0 - https://www.threadingbuildingblocks.org/faq/10"
     description = "Intel(R) Threading Building Blocks (Intel(R) TBB) lets you easily write parallel C++ programs that take full advantage of multicore performance, that are portable, composable and have future-proof scalability."
     url = "https://github.com/odant/conan-tbb"
-    settings = {
-        "os": ["Windows", "Linux"],
-        "compiler": ["Visual Studio", "gcc"],
-        "build_type": ["Debug", "Release"],
-        "arch": ["x86_64", "x86", "mips", "armv7"]
-    }
+    settings = "os", "compiler", "build_type", "arch"
     options = {
         "dll_sign": [False, True],
         "ninja": [False, True],
         "shared": [True, False],
         "atomic_wait": [True, False]
     }
-    default_options = "dll_sign=True", "ninja=True", "shared=True", "atomic_wait=False"
-    generators = "cmake"
+    default_options = {
+        "dll_sign":    True, 
+        "ninja":       True,
+        "shared":      True, 
+        "atomic_wait": False
+    }
     exports_sources = "src/*", "CMakeLists.txt", \
                       "test_global_control-two-core.patch", \
                       "dynamic_winapi.patch", \
-                      "add_atomic_wait.patch", \
-                      "FindTBB.cmake"
-    no_copy_source = True
+                      "add_atomic_wait.patch"
+    #no_copy_source = True
     build_policy = "missing"
+    python_requires = "windows_signtool/[>=1.2]@odant/stable"
     
+    def layout(self):
+        tools.cmake.cmake_layout(self, src_folder="src")
+            
     def configure(self):
         # Only C++11
         if self.settings.compiler.get_safe("libcxx") == "libstdc++":
             raise ConanException("This package is only compatible with libstdc++11")
         # MT(d) static library
-        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
-            if self.settings.compiler.runtime == "MT" or self.settings.compiler.runtime == "MTd":
-                self.options.shared=False
+        if self.settings.os == "Windows" and self.settings.compiler == "msvc" and self.settings.compiler.runtime == "static":
+            self.options.shared = False
         # DLL sign, only Windows
         if self.settings.os != "Windows":
-            del self.options.dll_sign
+            self.options.rm_safe("dll_sign")
 
     def build_requirements(self):
         if self.options.get_safe("ninja"):
-            self.build_requires("ninja/[>=1.10.1]")
-        if get_safe(self.options, "dll_sign"):
-            self.build_requires("windows_signtool/[>=1.1]@%s/stable" % self.user)
+            self.tool_requires("ninja/[>=1.12.1]")
 
-    def source(self):
-        tools.patch(patch_file="test_global_control-two-core.patch")
+    def generate(self):
+        benv = tools.env.VirtualBuildEnv(self)
+        benv.generate()
+        if tools.microsoft.is_msvc(self):
+            vc = tools.microsoft.VCVars(self)
+            vc.generate()
+        generator = "Ninja" if self.options.ninja == True else None
+        tc = tools.cmake.CMakeToolchain(self, generator=generator)
+        tc.variables["TBB_TEST"] = "OFF"
+        if self.settings.os == "Windows" and self.settings.arch == "x86":
+            tc.variables["TBB_STRICT"] = "OFF"
+        tc.generate()
+
+    def _patch_sources(self):
+        self.output.info(f"self.source_folder = {self.source_folder}")
+        tools.files.patch(self, patch_file="test_global_control-two-core.patch")
         if self.options.get_safe("atomic_wait"):
-            tools.patch(patch_file="add_atomic_wait.patch")
+            tools.files.patch(self, patch_file=os.path.join(self.source_folder,"add_atomic_wait.patch"))
         if self.settings.os == "Windows":
-            tools.patch(patch_file="dynamic_winapi.patch")
+            tools.files.patch(self, patch_file="dynamic_winapi.patch")
 
     def build(self):
-        build_type = "RelWithDebInfo" if self.settings.build_type == "Release" else "Debug"
-        generator = "Ninja" if self.options.ninja == True else None
-        cmake = CMake(self, build_type=build_type, generator=generator)
-        cmake.verbose = False
-        cmake.definitions["TBB_TEST:BOOL"] = "OFF"
-        if self.settings.os == "Windows" and self.settings.arch == "x86":
-            cmake.definitions["TBB_STRICT:BOOL"] = "OFF"
+        self._patch_sources()
+        cmake = tools.cmake.CMake(self)
         cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("FindTBB.cmake", src=".", dst=".")
-        self.copy("*.h", src="src/include", dst="include", keep_path=True)
-        
-        self.copy("lib/tbb12.lib", dst="lib", keep_path=False)
-        self.copy("lib/tbb12_debug.lib", dst="lib", keep_path=False)
-        self.copy("bin/tbb12.dll", dst="bin", keep_path=False)
-        self.copy("bin/tbb12_debug.dll", dst="bin", keep_path=False)
-        self.copy("*/tbb12.pdb", dst="bin", keep_path=False)
-        self.copy("*/tbb12_debug.pdb", dst="bin", keep_path=False)
-        
-        self.copy("*/libtbb.so*", dst="lib", keep_path=False, symlinks=True)
-        self.copy("*/libtbb_debug.so*", dst="lib", keep_path=False, symlinks=True)
-
+        cmake = tools.cmake.CMake(self)
+        cmake.install()
+        tools.files.rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        tools.files.rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        tools.files.rm(self, "*tbbmalloc*", os.path.join(self.package_folder, "bin"))
+        tools.files.rm(self, "*tbbmalloc*", os.path.join(self.package_folder, "lib"))
+        if self.settings.os == "Windows":
+            tools.files.rm(self, "*tbb.*", os.path.join(self.package_folder, "lib"))
+            tools.files.rm(self, "*tbb_*", os.path.join(self.package_folder, "lib"))
+            
         # Sign DLL
-        if get_safe(self.options, "dll_sign"):
-            import windows_signtool
-            pattern = os.path.join(self.package_folder, "bin", "*.dll")
-            for fpath in glob.glob(pattern):
-                fpath = fpath.replace("\\", "/")
-                for alg in ["sha1", "sha256"]:
-                    is_timestamp = True if self.settings.build_type == "Release" else False
-                    cmd = windows_signtool.get_sign_command(fpath, digest_algorithm=alg, timestamp=is_timestamp)
-                    self.output.info("Sign %s" % fpath)
-                    self.run(cmd)
+        if self.options.get_safe("dll_sign"):
+            self.python_requires["windows_signtool"].module.sign(self, [os.path.join(self.package_folder, "bin", "*.dll")])
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_file_name", "TBB")
+        self.cpp_info.set_property("cmake_target_name", "TBB::tbb")
+        self.cpp_info.libs = tools.files.collect_libs(self)
         # Disable auto link
         if self.settings.os == "Windows":
             self.cpp_info.defines.append("__TBB_NO_IMPLICIT_LINKAGE=1")
